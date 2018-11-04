438210a107da40ca15ceea17fe210f6a0d2425d7
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2018-11-04 12:42:51 +0800

    Add files via upload

--- gui_action.m
+++ gui_action.m
@@ -1,6 +1,68 @@
 
+if button_action==0
+    try
+    [File,PATHNAME] = uigetfile('*.wav');
+    catch
+    end
+    
+    if PATHNAME~=0
+        if strcmp(Recording_device,'SM')==1
+            temp=strfind(File,'_20'); temp=temp(1);
+            label_pos=[1 4; 5 6; 7 8; 10 11; 12 13; 14 15]+temp;
+            extra_label=[];
+        elseif strcmp(Recording_device,'Soundtrap')==1
+            temp=strfind(File,'.'); temp=temp(1);
+            label_pos=[1 2; 3 4; 5 6; 7 8; 9 10; 11 12]+temp;
+            extra_label=[20];
+        else
+            label_pos=zeros(6,2);
+            extra_label=[];
+        end
+    else 
+        label_pos=zeros(6,2);
+        extra_label=[];
+    end
+    
+    hlabel=uicontrol('Style','text','String',File,'FontSize',15,'FontWeight','bold','FontName','FixedWidth','Position',[350 400 400 30],'HorizontalAlignment','left'); 
+    reference=[]; for n=1:length(File); temp=num2str(100+n); reference=[reference temp(end)]; end
+    hlabel=uicontrol('Style','text','String',[reference],'FontSize',15,'FontName','FixedWidth','Position',[350 370 400 30],'HorizontalAlignment','left'); 
+    
+    h_y1=uicontrol('Style','edit','String', label_pos(1,1),'FontSize',11,'Position',[500 350 50 20]);
+    h_y2=uicontrol('Style','edit','String', label_pos(1,2),'FontSize',11,'Position',[570 350 50 20]);
+    h_m1=uicontrol('Style','edit','String', label_pos(2,1),'FontSize',11,'Position',[500 310 50 20]);
+    h_m2=uicontrol('Style','edit','String', label_pos(2,2),'FontSize',11,'Position',[570 310 50 20]);
+    h_d1=uicontrol('Style','edit','String', label_pos(3,1),'FontSize',11,'Position',[500 270 50 20]);
+    h_d2=uicontrol('Style','edit','String', label_pos(3,2),'FontSize',11,'Position',[570 270 50 20]);
+    h_H1=uicontrol('Style','edit','String', label_pos(4,1),'FontSize',11,'Position',[500 230 50 20]);
+    h_H2=uicontrol('Style','edit','String', label_pos(4,2),'FontSize',11,'Position',[570 230 50 20]);
+    h_M1=uicontrol('Style','edit','String', label_pos(5,1),'FontSize',11,'Position',[500 190 50 20]);
+    h_M2=uicontrol('Style','edit','String', label_pos(5,2),'FontSize',11,'Position',[570 190 50 20]);
+    h_S1=uicontrol('Style','edit','String', label_pos(6,1),'FontSize',11,'Position',[500 150 50 20]);
+    h_S2=uicontrol('Style','edit','String', label_pos(6,2),'FontSize',11,'Position',[570 150 50 20]);
+    h_y_extra=uicontrol('Style','edit','String', extra_label,'FontSize',11,'Position',[650 110 50 20]);
 
-if button_action==1
+elseif button_action==0.5
+    try
+        [parameter_file,parameter_path] = uigetfile('*.mat');
+    catch
+    end
+    cd(parameter_path); load(parameter_file); cd(PATHNAME_o);
+    if strcmp(Recording_env,'Wat')==1
+        env_control=1;
+    elseif strcmp(Recording_env,'Air')==1
+        env_control=2;
+    end
+    h_edit1=uicontrol('Style','popupmenu','String', '1|2','Value',channel,'FontSize',10,'Position',[225 385 100 30]);
+    h_edit2=uicontrol('Style','popupmenu','String', 'Wat|Air','Value',env_control,'FontSize',10,'Position',[225 345 100 30]);
+    h_edit3=uicontrol('Style','edit','String', sen,'FontSize',12,'Position',[225 305 100 30]);
+    h_edit4=uicontrol('Style','edit','String', reading_interval,'FontSize',12,'Position',[225 265 100 30]);
+    h_edit5=uicontrol('Style','edit','String', ltsa_resolution,'FontSize',12,'Position',[225 225 100 30]);
+    h_edit6=uicontrol('Style','edit','String', FFT_size,'FontSize',12,'Position',[225 185 100 30]);
+    h_edit7=uicontrol('Style','edit','String', max(Frequency_range),'FontSize',12,'Position',[225 145 100 30]);
+    h_edit8=uicontrol('Style','edit','String', min(Frequency_range),'FontSize',12,'Position',[225 105 100 30]);
+    h_edit9=uicontrol('Style','edit','String', outfile,'FontSize',12,'Position',[225 65 200 30]);
+    
+elseif button_action==1
     outfile=get(h_edit9,'String');
     
     % Analysis setting
